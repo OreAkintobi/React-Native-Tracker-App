4239373a70eabcc13fdfeac8b1887305d92c288e
OreAkintobi <oreakintobi@gmail.com>
2020-04-28 16:39:29 +0100

    added user token to state, more error handling

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,32 +1,34 @@
 import createDataContext from "./createDataContext";
 import trackerApi from "../api/tracker";
+import { AsyncStorage } from "react-native";
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case "add_error":
       return { ...state, errorMessage: action.payload };
+    case "signup":
+      return { errorMessage: "", token: action.payload };
     default:
       return state;
   }
 };
 
-const signup = (dispatch) => {
-  return async ({ email, password }) => {
+const signup = (dispatch) => async ({ email, password }) => {
+  try {
     // make api request to sign up with email & password
-    try {
-      const response = await trackerApi.post("/signup", { email, password });
-      // console.log(response.data);
-    } catch (error) {
-      // console.log(error.response.data);
-      dispatch({
-        type: "add_error",
-        payload: "Something went wrong with sign up",
-      });
-    }
+    const response = await trackerApi.post("/signup", { email, password });
     // if we sign up, modify our state, and say we are authenticated
-
+    await AsyncStorage.setItem("token", response.data.token);
+    dispatch({ type: "signup", payload: response.data.token });
+    // console.log(response.data);
+  } catch (error) {
     // if signing up fails, we need to handle that error
-  };
+    // console.log(error.response.data);
+    dispatch({
+      type: "add_error",
+      payload: "Something went wrong with sign up",
+    });
+  }
 };
 
 const signin = (dispatch) => {
@@ -46,5 +48,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = createDataContext(
   authReducer,
   { signin, signout, signup },
-  { isSignedIn: false, errorMessage: "" }
+  { token: null, errorMessage: "" }
 );
