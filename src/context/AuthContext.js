82dd91702ce9407f972628d26edd002470fb7e65
OreAkintobi <oreakintobi@gmail.com>
2020-04-28 15:57:51 +0100

    added signup route, function in tracker, screen

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from "./createDataContext";
+import trackerApi from "../api/tracker";
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -8,9 +9,16 @@ const authReducer = (state, action) => {
 };
 
 const signup = (dispatch) => {
-  return ({ email, password }) => {
+  return async ({ email, password }) => {
     // make api request to sign up with email & password
+    try {
+      const response = await trackerApi.post("/signup", { email, password });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
     // if we sign up, modify our state, and say we are authenticated
+
     // if signing up fails, we need to handle that error
   };
 };
@@ -31,6 +39,6 @@ const signout = (dispatch) => {
 
 export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
+  { signin, signout, signup },
   { isSignedIn: false }
 );
