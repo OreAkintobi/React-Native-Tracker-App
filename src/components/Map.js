235b48d67b51e6efb2bc178f34d4ea334b63b5fb
OreAkintobi <oreakintobi@gmail.com>
2020-04-29 13:48:14 +0100

    integrated google map and added polyline to map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,18 +1,36 @@
 import React from "react";
 import { StyleSheet, Text } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Polyline } from "react-native-maps";
 
 const Map = () => {
+  let points = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      points.push({
+        latitude: 6.43845 + i * 0.001,
+        longitude: 3.524577 + i * 0.001,
+      });
+    } else {
+      points.push({
+        latitude: 6.43845 - i * 0.0000001,
+        longitude: 3.524577 + i * 0.001,
+      });
+    }
+  }
+
   return (
     <MapView
       style={styles.map}
-      initialRegion={{
-        latitude: 37.33233,
-        longitude: -122.03121,
+      provider="google"
+      region={{
+        latitude: 6.43845,
+        longitude: 3.524577,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-    />
+    >
+      <Polyline coordinates={points} />
+    </MapView>
   );
 };
 
