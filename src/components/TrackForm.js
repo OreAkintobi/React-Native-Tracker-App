844813b3c834bb01e0c7a782ffdc66792a082b0d
OreAkintobi <oreakintobi@gmail.com>
2020-05-02 23:46:39 +0100

    added location context and conditional styling

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,15 +1,36 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "./Spacer";
+import { Context as LocationContext } from "../context/LocationContext";
 
 const TrackForm = () => {
+  const {
+    state: { name, recording, locations },
+    startRecording,
+    stopRecording,
+    changeName,
+  } = useContext(LocationContext);
+
+  // console.log(locations.length);
   return (
     <>
       <Spacer>
-        <Input placeholder="Enter Track Name" />
+        <Input
+          placeholder="Enter Track Name"
+          onChangeText={changeName}
+          value={name}
+        />
       </Spacer>
-      <Button title="Start Recording" />
+      {recording ? (
+        <Button
+          title="Stop Recording"
+          onPress={stopRecording}
+          buttonStyle={{ backgroundColor: "red" }}
+        />
+      ) : (
+        <Button title="Start Recording" onPress={startRecording} />
+      )}
     </>
   );
 };
